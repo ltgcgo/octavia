728d1a2a021c932223b22bbca22fa30d80133b14
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2023-02-02 15:44:59 +0000

    Text interface is now in a separate file.

--- src/disp/common.js
+++ src/disp/common.js
@@ -1,3 +1,29 @@
 "use strict";
 
-// The common values used in all example displays.
+// The common values used in all example screens.
+
+let noteNames = [
+	"C~", "C#", "D~", "Eb",
+	"E~", "F~", "F#", "G~",
+	"Ab", "A~", "Bb", "B~"
+], noteRegion = "!0123456789",
+hexMap = "0123456789ABCDEF",
+map = "0123456789_aAbBcCdDeEfFgGhHiIjJ-kKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ",
+waveMap = ["-", "~", "+", "|"];
+
+const bgOrange = "#ffaa2264",
+bgGreen = "#aaff2264",
+bgWhite = "#b3d8de64",
+bgRed = "#ff798664";
+
+export {
+	noteNames,
+	noteRegion,
+	hexMap,
+	map,
+	waveMap,
+	bgOrange,
+	bgGreen,
+	bgWhite,
+	bgRed
+};
