364ccd2f63cb9fe272c7370557ce6c3b5e57d68b
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2023-02-03 10:13:06 +0000

    Updating middleware conf page UI design.

--- src/demoMw/index.js
+++ src/demoMw/index.js
@@ -10,20 +10,42 @@ import {
 	SimpleMidiEventEmitter
 } from "../bridge/index.mjs";
 
+let inPortMap = {};
+
+let inputConv = function (ev) {
+	midiLine.postMessage(toJson(ev.data, inPortMap[ev.target.id]));
+};
+let inputConn = function () {
+	let inputPort = midiAccess.inputs.get(this.getAttribute("mw-port-id"));
+	inPortMap[inputPort.id] = 0;
+	inputPort.open();
+	inputPort.addEventListener("midimessage", inputConv);
+};
+
 (async function () {
 	self.midiAccess = await navigator.requestMIDIAccess({"sysex": true, "software": true});
 	self.fromJson = fromJson;
 	self.toJson = toJson;
 	self.MEE = SimpleMidiEventEmitter;
 	self.inBridge = getBridge();
+	let midiInBox = $e("#midi-in-list"), midiOutBox = $e("#midi-out-list");
 	midiAccess.addEventListener("statechange", (ev) => {
-		console.info(ev.port);
+		console.debug(ev.port);
 	});
 	midiAccess.inputs.forEach((port, id) => {
-		console.info(`Discovered input ${id}: ${port.name}.`);
+		let midin = document.createElement("li");
+		midin.innerText = port.name;
+		midin.id = `mw-in-${id}`;
+		midin.setAttribute("mw-port-id", id);
+		midin.addEventListener("click", inputConn);
+		midiInBox.appendChild(midin);
 	});
 	midiAccess.outputs.forEach((port, id) => {
-		console.info(`Discovered output ${id}: ${port.name}.`);
+		let midout = document.createElement("li");
+		midout.innerText = port.name;
+		midout.id = `mw-in-${id}`;
+		midout.setAttribute("mw-port-id", id);
+		midiOutBox.appendChild(midout);
 	});
 	self.midiLine = getBridge();
 	const propsMid = JSON.parse('{"extensions":[".mid",".MID",".kar",".KAR",".syx",".SYX"],"startIn":"music","id":"midiOpener","description":"Open a MIDI file"}');
--- src/fakeMu/index.js
+++ src/fakeMu/index.js
@@ -416,7 +416,7 @@ self.performance = currentPerformance;
 	// MU128 demo
 	let perf = new TimedEvents();
 	// Disable native RS
-	perf.push(new PointEvent(0, {type: 15, track: 0, data: [67, 16, 73, 0, 0, 14, 0]})); 
+	perf.push(new PointEvent(0, {type: 15, track: 0, data: [67, 16, 73, 0, 0, 14, 0]}));
 	perf.push(new PointEvent(0, generateSwitch(0, 0, 0)));
 	perf.push(new PointEvent(1.6, generateSwitch(0, 0, 3)));
 	perf.push(new PointEvent(40.02, generateSwitch(48, 3, 3)));
