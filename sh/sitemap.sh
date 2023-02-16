a269cfef3c43dfb00de9ed49770201181b880935
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2023-02-16 18:08:30 +0000

    XML sitemap generation.

--- sh/sitemap.sh
+++ sh/sitemap.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-rm ghp/map.txt
 domain=$(cat conf/domain.txt)
+printf '<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' > ghp/sitemap.xml
 cat conf/sitemap.txt | while IFS= read -r path ; do
-	echo "${domain}${path}" >> ghp/map.txt
+	printf "<url><loc>${domain}${path//index.htm/}</loc><lastmod>$(date -u '+%Y-%m-%d %H:%M:%S' -r .${path})</lastmod></url>" >> ghp/sitemap.xml
 done
+printf '</urlset>' >> ghp/sitemap.xml
 exit
