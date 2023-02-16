c7bc1f19052537bc95eb5d92fd463c5f9fe5944e
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2023-02-16 14:12:44 +0000

    SEO test.

--- src/cambiare_wa/index.js
+++ src/cambiare_wa/index.js
@@ -1,1 +1,66 @@
 "use strict";
+
+import {} from "../../libs/lightfelt@ltgcgo/main/cssClass.js";
+import {$e, $a} from "../../libs/lightfelt@ltgcgo/main/quickPath.js";
+import Cambiare from "../cambiare/index.mjs";
+import {fileOpen} from "../../libs/browser-fs-access@GoogleChromeLabs/browser_fs_access.min.js";
+import {
+	getBridge
+} from "../bridge/index.mjs";
+
+let demoBlobs = {};
+let demoPerfs = {};
+let demoInfo = {};
+let demoModes = [];
+demoModes[9] = "gm";
+let currentPerformance;
+let currentAnimation;
+let useMidiBus = false;
+
+// Standard demo switching
+let stDemo = $a("b.demo");
+stDemo.to = function (i) {
+	stDemo.forEach(function (e) {
+		e.classList.off("active");
+	});
+	if (i > -1) {
+		stDemo[i].classList.on("active");
+	};
+};
+stDemo.forEach(function (e, i, a) {
+	e.addEventListener("click", async function () {
+		audioPlayer.pause();
+		if (!demoBlobs[e.title]?.midi) {
+			demoBlobs[e.title] = {};
+			audioPlayer.src = "about:blank";
+			demoBlobs[e.title].midi = await (await fetch(`./demo/${e.title}.mid`)).blob();
+			demoBlobs[e.title].wave = await (await fetch(`./demo/${e.title}.opus`)).blob();
+		};
+		currentPerformance = demoPerfs[e.title];
+		currentPerformance?.resetIndex();
+		currentAnimation = demoInfo[e.title];
+		audioPlayer.currentTime = 0;
+		visualizer.reset();
+		visualizer.loadFile(demoBlobs[e.title].midi);
+		if (audioBlob) {
+			URL.revokeObjectURL(audioBlob);
+		};
+		audioBlob = demoBlobs[e.title].wave;
+		audioPlayer.src = URL.createObjectURL(audioBlob);
+		if (demoModes[i]?.length > 0) {
+			visualizer.switchMode(demoModes[i]);
+		};
+		stDemo.to(i);
+	});
+});
+
+// Start the visualizers
+self.visualizer = new Cambiare();
+visualizer.addEventListener("reset", function (e) {
+	console.info("Processor reset.");
+});
+
+// Listen to mode switches
+visualizer.addEventListener("mode", function (ev) {
+	stSwitch.to(stSwitchMode.indexOf(ev.data));
+});
