6c7f910f7c93290918d9ec7ce8b95e135d301d34
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2023-02-16 14:12:44 +0000

    SEO test.

--- src/cambiare/index.mjs
+++ src/cambiare/index.mjs
@@ -1,1 +1,7 @@
 "use strict";
+
+import {CustomEventSource} from "../../libs/lightfelt@ltgcgo/ext/customEvents.js";
+
+let Cambiare = class Cambiare extends CustomEventSource {};
+
+export default Cambiare;
