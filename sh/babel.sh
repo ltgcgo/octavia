0578a23a6724e7a8e93081a8f67fdd3c9e1597d3
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2024-02-15 10:41:42 +0000

    Manual XP transpilation is now the past.

--- sh/babel.sh
+++ sh/babel.sh
@@ -3,6 +3,7 @@
 cat conf/babelTargets.txt | while IFS= read -r target; do
 	realTarget="./dist/${target}"
 	if [ -f "$realTarget" ]; then
+		echo "Now building \"${target}\" ..."
 		deno run --allow-read --allow-write utils/babel/build.js "dist/${target}"
 	else
 		echo "Target \"${target}\" does not exist."
