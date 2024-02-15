58163a8795bf8c43c06e4af54b3a9d587b35c308
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2024-02-15 10:35:16 +0000

    Babel is now working.

--- utils/babel/build.js
+++ utils/babel/build.js
@@ -2,21 +2,14 @@
 "use strict";
 
 import Babel from "../../libs/babel/standalone.js";
-console.debug(Babel);
+//console.debug(Babel);
 
 if (!Deno.args[0]) {
 	throw(`No build target was provided!`);
 };
 
 let code = await Deno.readTextFile(Deno.args[0]),
-legacy;
-let opt = JSON.parse(await Deno.readTextFile(`../../conf/babel.json`));
-await Babel.transform(code, opt, (err, result) => {
-	if (err) {
-		console.error(`The Babel transpiler has encountered an error.`);
-		console.error(err);
-	} else {
-		legacy = result.code;
-	};
-});
+opt = JSON.parse(await Deno.readTextFile(`./conf/babel.json`)),
+result = Babel.transform(code, opt);
+let legacy = result.code;
 await Deno.writeTextFile(Deno.args[0], legacy);
