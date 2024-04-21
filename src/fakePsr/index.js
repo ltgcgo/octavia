04d2c5e15ae5194b3becce932b48358e7151d721
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2024-04-21 15:09:04 +0000

    Migrated generateSwitch in PSR to the new SysEx.

--- src/fakePsr/index.js
+++ src/fakePsr/index.js
@@ -26,13 +26,14 @@ let useMidiBus = false;
 let demoId = 0;
 
 // Generate Octavia channel switch SysEx
-let generateSwitch = function (ch = 0, min, max) {
-	let data = [67, 16, 73, 0, 0, 64, ch];
-	if (min?.constructor == Number) {
-		data.push(min);
-		if (max.constructor == Number) {
-			data.push(max);
-		};
+self.generateSwitch = function (ch = 0, min, max) {
+	if (min != undefined && max == undefined) {
+		console.warn(`Invalid bounds for channel switch generation.`);
+		return;
+	};
+	let data = [67, 16, 73, 11, 0, 0, ch];
+	if (min != undefined) {
+		data.push(Math.floor(Math.log2(max - min + 1)), min);
 	};
 	return {
 		type: 15,
