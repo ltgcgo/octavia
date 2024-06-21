88ca7c75ff5bfdfc26d37f9a19cb955410337609
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2024-06-21 15:19:11 +0000

    Foundation of a TSV viewer.

--- sh/srcMap.sh
+++ sh/srcMap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd test/js
 ls -1 ../../dist/*js | while IFS= read -r file; do
-	ln -s "$file"
-	ln -s "$file".map
+	ln -s "$file" 2>/dev/null
+	ln -s "$file".map 2>/dev/null
 done
 exit
---
+++ src/tsvView/index.js
@@ -0,0 +1,7 @@
+"use strict";
+
+import {} from "../../libs/lightfelt@ltgcgo/main/cssClass.js";
+import {$e, $a} from "../../libs/lightfelt@ltgcgo/main/quickPath.js";
+import {fileOpen} from "../../libs/browser-fs-access@GoogleChromeLabs/browser_fs_access.min.js";
+
+const propsTsv = JSON.parse(`{"extensions":[".tsv",".TSV"],"startIn":"pictures","id":"tsvOpener","description":"Open a TSV file"}`);
