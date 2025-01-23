ecae75171aa16a66f265cd87a808d113364f61ac
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2025-01-23 20:55:10 +0000

    Experimental pages build.

---
+++ sh/ghGenerate.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+COMPRESS_CRIT="\.(bin|bm|bmp|conf|css|csv|htm|html|ico|js|json|kar|list|lst|map|md|mid|mjs|mod|otf|svg|tsv|ttf|vgm|wasm|webmanifest|xml)$"
+echo "$(date +"%s")" > build-time.txt
+cp -Lrv ghp ghp-gz
+cp -Lrv ghp ghp-br
+cd ghp
+tar cvf ../pages-build.tar *
+cd ..
+zopfli --i1 -v pages-build.tar
+rm -v pages-build.tar
+cd ghp-gz
+tree -ifl | grep -E "${COMPRESS_CRIT}" | while IFS= read -r file; do
+	zopfli --i1 "$file"
+	if [ -f "$file" ]; then
+		rm -v "$file"
+	fi
+done
+tar cvf ../pages-build-gz.tar *
+cd ..
+cd ghp-br
+tree -ifl | grep -E "${COMPRESS_CRIT}" | while IFS= read -r file; do
+	brotli -vq 11 "$file"
+	if [ -f "$file" ]; then
+		rm -v "$file"
+	fi
+done
+tar cvf ../pages-build-br.tar *
+cd ..
+exit
