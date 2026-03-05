3d55727297aaf086e07abc1d0e3a93ec55929675
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2026-03-05 11:21:48 +0000

    Added a TSV to JSON converter.

--- src/chord/generate.js
+++ src/chord/generate.js
@@ -3,6 +3,20 @@
 import TextReader from "../../libs/rochelle@ltgcgo/textRead.mjs";
 import DSVParser from "../../libs/rochelle@ltgcgo/dsvParse.mjs";
 
+let finalObj = [];
+
 for await (let line of DSVParser.parseObjects(0, TextReader.line((await Deno.open("./src/chord/chords.tsv")).readable))) {
-	console.debug(line);
+	let tmpObj = {};
+	if (line.id && line.sp) {
+		tmpObj.id = parseInt(line.id, 16);
+		tmpObj.sp = line.sp.split(",");
+		if (line.xf) {
+			tmpObj.xf = parseInt(line.xf, 16);
+		};
+	};
+	finalObj.push(tmpObj);
 };
+
+//console.debug(finalObj);
+
+await Deno.writeTextFile("./src/chord/generated/chords.json", JSON.stringify(finalObj));
