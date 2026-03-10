f15ae6dd8c354c0b9a0d48c29c1533416efda65c
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2026-03-10 07:21:10 +0000

    Generated chord display bitfield for PSR-170.

--- src/data/generate.js
+++ src/data/generate.js
@@ -3,9 +3,9 @@
 import TextReader from "../../libs/rochelle@ltgcgo/textRead.mjs";
 import DSVParser from "../../libs/rochelle@ltgcgo/dsvParse.mjs";
 
+// Master chord map
 let finalObj = [];
 let spToId = new Map();
-
 for await (let line of DSVParser.parseObjects(0, TextReader.line((await Deno.open("./src/data/chords.tsv")).readable))) {
 	let tmpObj = {};
 	if (line.id && line.sp) {
@@ -20,14 +20,12 @@ for await (let line of DSVParser.parseObjects(0, TextReader.line((await Deno.ope
 	};
 	finalObj.push(tmpObj);
 };
-
 //console.debug(finalObj);
-
 await Deno.writeTextFile("./src/data/generated/chords.json", JSON.stringify(finalObj));
 
+// QY chord plan
 //let planSet1 = new Set(), planSet2 = new Set();
 let qyPlan = [];
-
 for await (let line of DSVParser.parseObjects(0, TextReader.line((await Deno.open("./src/data/qyChordPlan.tsv")).readable))) {
 	let plan = [spToId.get(line.chord), `m${line.main ?? ""}`];
 	if (line.main) {
@@ -41,3 +39,12 @@ for await (let line of DSVParser.parseObjects(0, TextReader.line((await Deno.ope
 };
 //console.debug(planSet1, planSet2);
 await Deno.writeTextFile("./src/data/generated/qyChordPlan.json", JSON.stringify(qyPlan));
+
+// PSR-170 chord plan
+let psr170Plan = [];
+for await (let line of DSVParser.parseObjects(0, TextReader.line((await Deno.open("./src/data/psr170ChordPlan.tsv")).readable))) {
+	let plan = [spToId.get(line.chord), parseInt(line.bitfield, 16)];
+	psr170Plan.push(plan);
+};
+//console.debug(psr170Plan);
+await Deno.writeTextFile("./src/data/generated/psr170ChordPlan.json", JSON.stringify(psr170Plan));
