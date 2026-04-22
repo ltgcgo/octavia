1fc00718bab7d5e8857c23c7f8a4e5cc94d53774
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2026-04-22 13:39:28 +0000

    Benchmarked `DataView` against `TypedArray`. - `DataView` is normally slight slower than `TypedArray`. (1.03~1.45×) - `IntegerHandler` is consistently worse across the board. (~18.71×)

--- utils/bench/tadv.js
+++ utils/bench/tadv.js
@@ -1,3 +1,4 @@
+import {IntegerHandler} from "../../libs/seamstress@ltgcgo/index.mjs";
 import liteBench from "./liteBench.mjs";
 
 let benchmark;
@@ -9,6 +10,7 @@ if (self.Deno) {
 
 let dummyArray = new Uint8Array(1024);
 let dummyDv = new DataView(dummyArray.buffer);
+let dummyAi32 = new Int32Array(dummyArray.buffer);
 
 benchmark(function warmUp() {
 	return Math.log2(Math.random());
@@ -29,5 +31,29 @@ benchmark(function u8TypedArray() {
 	let dummyVar;
 	for (let i = 0; i < dummyArray.length; i ++) {
 		dummyVar = dummyArray[i];
+	};
+});
+benchmark(function i32DataViewA() {
+	let dummyVar;
+	for (let i = 0; i < dummyDv.byteLength; i += 4) {
+		dummyVar = dummyDv.getInt32(i);
+	};
+});
+benchmark(function i32DataViewB() {
+	let dummyVar;
+	for (let i = 0; (i << 2) < dummyDv.byteLength; i ++) {
+		dummyVar = dummyDv.getInt32(i << 2);
+	};
+});
+benchmark(function i32TypedArrayA() {
+	let dummyVar;
+	for (let i = 0; i < dummyAi32.length; i ++) {
+		dummyVar = dummyAi32[i];
+	};
+});
+benchmark(function i32TypedArrayB() {
+	let dummyVar;
+	for (let i = 0; (i << 2) < dummyArray.length; i ++) {
+		dummyVar = IntegerHandler.readInt32(dummyArray, false, i);
 	};
 });
