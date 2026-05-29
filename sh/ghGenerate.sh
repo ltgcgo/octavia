1b39d9148f978e45a174229564d465e7c525514b
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2026-05-29 08:11:04 +0000

    Pages: Allowed conditional long-term compression.

--- sh/ghGenerate.sh
+++ sh/ghGenerate.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 COMPRESS_CRIT="\.(ass|atom|bin|bm|bmp|conf|css|csv|htm|html|ico|js|json|kar|list|lrc|lst|map|md|mid|mjs|mod|mts|otf|rss|sbv|srt|ssa|svg|trc|ts|tsv|ttf|ttml|txt|vgm|vtt|wasm|webmanifest|xml|ytt)$"
 
-#if [ ! -f "$(which zopfli)" ]; then
-	#sudo apt install -y zopfli
-#fi
+if [ "$1" != "" ]; then
+	if [ ! -f "$(which zopfli)" ]; then
+		sudo apt install -y zopfli
+	fi
+fi
 #if [ ! -f "$(which tree)" ]; then
 	#sudo apt install -y tree
 #fi
@@ -12,7 +14,7 @@ echo "$(date +"%s")" > build-time.txt
 cp -Lr ghp ghp-raw
 cd ghp-raw
 printf "" > ../fileHashes.tsv
-find . -type f | while IFS= read -r file; do
+tree -ifl | while IFS= read -r file; do
 	if [ -f "$file" ]; then
 		# Is a file
 		fileHash="$(sha256sum "${file}" | cut -d' ' -f1)"
@@ -28,7 +30,7 @@ find . -type f | while IFS= read -r file; do
 		fi
 	fi
 done
-tar cf ../pages-build.tar .
+tar cf ../pages-build.tar *
 echo "Built uncompressed files: $(wc -c ../pages-build.tar | cut -d' ' -f1) B"
 cd ..
 #zopfli --i1 -v pages-build.tar
@@ -38,7 +40,7 @@ rm -r ghp-raw
 cp -Lr ghp ghp-base
 cd ghp-base
 printf "" > ../fileHashes.tsv
-find . -type f | while IFS= read -r file; do
+tree -ifl | while IFS= read -r file; do
 	if [ -f "$file" ]; then
 		# Is a file
 		if [ "$(echo "$file" | grep -E "$COMPRESS_CRIT")" != "" ]; then
@@ -58,23 +60,34 @@ find . -type f | while IFS= read -r file; do
 				rm "$file"
 			fi
 		else
-			echo "File \"${file}\" is preserved."
+			fileHash="$(sha256sum "${file}" | cut -d' ' -f1)"
+			findResult="$(grep -F "${fileHash}	" ../fileHashes.tsv | cut -d '	' -f2)"
+			if [ "$findResult" != "" ] ; then
+				pathDiffRaw="$(realpath -Lsm --relative-to="${file}" "${findResult}")"
+				pathDiff="${pathDiffRaw/\.\.\//}"
+				echo "Deduplicated: ${file} -> ${pathDiff}"
+				rm "${file}"
+				ln -s "${pathDiff}" "${file}"
+			else
+				echo "${fileHash}	$(realpath -s "${file}")" >> ../fileHashes.tsv
+				echo "File \"${file}\" is preserved."
+			fi
 		fi
 	fi
 done
-find . -type d | while IFS= read -r folder; do
+tree -ifld | while IFS= read -r folder; do
 	if [ -d "$folder" ]; then
 		rmdir -p "$folder" 2>/dev/null
 	fi
 done
-tar cf ../pages-build-base.tar .
+tar cf ../pages-build-base.tar *
 echo "Built incompressible files: $(wc -c ../pages-build-base.tar | cut -d' ' -f1) B"
 cd ..
 rm -r ghp-base
 cp -Lr ghp ghp-gz
 cd ghp-gz
 printf "" > ../fileHashes.tsv
-find . -type f | while IFS= read -r file; do
+tree -ifl | while IFS= read -r file; do
 	if [ -f "$file" ]; then
 		# Is a file
 		if [ "$(echo "$file" | grep -E "$COMPRESS_CRIT")" != "" ]; then
@@ -88,7 +101,11 @@ find . -type f | while IFS= read -r file; do
 				rm "$file"
 			else
 				echo "${fileHash}	$(realpath -s "${file}")" >> ../fileHashes.tsv
-				gzip -9 "$file" && echo "Compressed \"${file}\" with Gzip."
+				if [ "$1" != "" ]; then
+					zopfli --i1 && echo "Compressed \"${file}\" with Zopfli."
+				else
+					gzip -9 "$file" && echo "Compressed \"${file}\" with Gzip."
+				fi
 			fi
 		#else
 			#echo "File \"${file}\" cannot be compressed."
@@ -98,20 +115,20 @@ find . -type f | while IFS= read -r file; do
 		fi
 	fi
 done
-find . -type d | while IFS= read -r folder; do
+tree -ifld | while IFS= read -r folder; do
 	if [ -d "$folder" ]; then
 		rmdir -p "$folder" 2>/dev/null
 	fi
 done
 #cat ../fileHashes.tsv
-tar cf ../pages-build-gz.tar .
+tar cf ../pages-build-gz.tar *
 echo "Built precompressed Gzip files: $(wc -c ../pages-build-gz.tar | cut -d' ' -f1) B"
 cd ..
 rm -r ghp-gz
 cp -Lr ghp ghp-br
 cd ghp-br
 printf "" > ../fileHashes.tsv
-find . -type f | while IFS= read -r file; do
+tree -ifl | while IFS= read -r file; do
 	if [ -f "$file" ]; then
 		# Is a file
 		if [ "$(echo "$file" | grep -E "$COMPRESS_CRIT")" != "" ]; then
@@ -125,7 +142,11 @@ find . -type f | while IFS= read -r file; do
 				rm "$file"
 			else
 				echo "${fileHash}	$(realpath -s "${file}")" >> ../fileHashes.tsv
-				brotli -v9j "$file"
+				if [ "$1" != "" ]; then
+					brotli -vjq 10 "$file"
+				else
+					brotli -v9j "$file"
+				fi
 			fi
 		#else
 			#echo "File \"${file}\" cannot be compressed."
@@ -135,13 +156,13 @@ find . -type f | while IFS= read -r file; do
 		fi
 	fi
 done
-find . -type d | while IFS= read -r folder; do
+tree -ifld | while IFS= read -r folder; do
 	if [ -d "$folder" ]; then
 		rmdir -p "$folder" 2>/dev/null
 	fi
 done
 #cat ../fileHashes.tsv
-tar cf ../pages-build-br.tar .
+tar cf ../pages-build-br.tar *
 echo "Built precompressed Brotli files: $(wc -c ../pages-build-br.tar | cut -d' ' -f1) B"
 cd ..
 rm -r ghp-br
