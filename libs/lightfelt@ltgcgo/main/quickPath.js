de01f28674b51efa8f594112d8cc32217f55d0c8
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2026-09-01 05:54:29 +0000

    - State: Moved buffer conversion in `utils`. - Tools: Basic interactivity.

--- libs/lightfelt@ltgcgo/main/quickPath.js
+++ libs/lightfelt@ltgcgo/main/quickPath.js
@@ -1,14 +1,24 @@
 "use strict";
 
+/** @param selector {string}
+* @param source {Document}
+* @returns {HTMLElement} */
 const $e = function $e (selector, source = document) {
 	return source?.querySelector(selector);
 };
+/** @param selector {string}
+* @param source {Document}
+* @returns {HTMLElement} */
 const $a = function $a (selector, source = document) {
 	return Array.from(source?.querySelectorAll(selector));
 };
+/** @param selector {string}
+* @returns {HTMLElement} */
 HTMLElement.prototype.$e = function $e (selector) {
 	return this.querySelector(selector);
 };
+/** @param selector {string}
+* @returns {HTMLElement} */
 HTMLElement.prototype.$a = function $a (selector) {
 	return this.querySelectorAll(selector);
 };
--- src/dasmSingle/index.js
+++ src/dasmSingle/index.js
@@ -7,10 +7,30 @@ import {
 	$e,
 	$a
 } from "../../libs/lightfelt@ltgcgo/main/quickPath.js";
+import {
+	bufferFrom,
+	bufferTo
+} from "../state/utils/bufferIo.mjs";
 
 self.Alpine = Alpine;
 
+// Elements
+/** @type {HTMLInputElement} */
+const inputRaw = $e("input#text-raw");
+/** @type {HTMLInputElement} */
+const inputMia = $e("input#text-mia");
+/** @type {HTMLDivElement} */
+const displayNakedEvent = $e("div#renderer-naked");
 
+self.gParseRaw = async () => {
+	const inputLength = inputRaw.value.length;
+	const normalisedInput = inputRaw.value.padEnd(inputLength + (inputLength & 1), "0");
+	const inputBuffer = bufferFrom("hex", normalisedInput);
+};
+self.gParseMia = async () => {
+	// WIP!
+	console.debug(inputMia.value);
+};
 
 (async () => {
 	Alpine.start();
