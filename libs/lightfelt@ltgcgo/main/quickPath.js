3664b9715ffab0b048685e260f48295dd5c5ac71
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2026-09-01 05:24:03 +0000

    Tool: Migrated some `sDasm` buttons to AlpineJS.

--- libs/lightfelt@ltgcgo/main/quickPath.js
+++ libs/lightfelt@ltgcgo/main/quickPath.js
@@ -1,10 +1,16 @@
 "use strict";
 
-let $e = function (selector, source = document) {
+const $e = function $e (selector, source = document) {
 	return source?.querySelector(selector);
 };
-let $a = function (selector, source = document) {
+const $a = function $a (selector, source = document) {
 	return Array.from(source?.querySelectorAll(selector));
+};
+HTMLElement.prototype.$e = function $e (selector) {
+	return this.querySelector(selector);
+};
+HTMLElement.prototype.$a = function $a (selector) {
+	return this.querySelectorAll(selector);
 };
 
 export {
---
+++ src/dasmSingle/index.js
@@ -0,0 +1,17 @@
+"use strict";
+
+import {
+	Alpine
+} from "../../libs/alpine@alpinejs/alpine.min.js";
+import {
+	$e,
+	$a
+} from "../../libs/lightfelt@ltgcgo/main/quickPath.js";
+
+self.Alpine = Alpine;
+
+
+
+(async () => {
+	Alpine.start();
+})();
