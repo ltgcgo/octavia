700fe20b29d5d37ba4c2f66f3cd7760ed4f5d9be
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2026-09-15 16:40:16 +0000

    Chores: Added other MIDI files in the LCR validator.

--- sh/validatorSetup.sh
+++ sh/validatorSetup.sh
@@ -17,5 +17,29 @@ cp -v "../download/artist/John Campbell/StarGame.mid.br" .
 cp -v "../download/artist/John Campbell/MOON_L.mid.br" .
 cp -v "../download/artist/John Campbell/BOP_U.mid.br" .
 cp -v "../download/artist/Lim Chong Voon/BLUE_P.mid.br" .
+cp -v "../download/vendor/gravis/ultrasound/gdvib6.mid.br" .
+cp -v "../download/vendor/gravis/ultrasound/the_rain.mid.br" .
+cp -v "../download/vendor/korg/ai2/05RWDEMO.mid.br" .
+cp -v "../download/vendor/korg/ai2/AGDEMO1.mid.br" .
+cp -v "../download/vendor/korg/ai2/AGDEMO2.mid.br" .
+cp -v "../download/vendor/korg/ai2/Korg - We've Got Dreams.mid.br" "./X5DDEMO2.mid.br"
+cp -v "../download/vendor/korg/ns5r/KORG - MissionMan.mid.br" "./MISSION.mid.br"
+cp -v "../download/vendor/microsoft/windows/onestop.mid.br" .
+cp -v "../download/vendor/roland/hypercanvas/04Orch.mid.br" .
+cp -v "../download/vendor/roland/sc-88/Y4002_03.mid.br" .
+cp -v "../download/vendor/roland/sc-88pro/11Gt_EFX.mid.br" .
+cp -v "../download/vendor/roland/sc-88pro/13Wah_G.mid.br" .
+cp -v "../download/vendor/roland/sc-8820/26orchst.mid.br" .
+cp -v "../download/vendor/roland/sc-8850/06DRUM.mid.br" .
+cp -v "../download/vendor/roland/sd-20/Demo04.mid.br" .
+cp -v "../download/vendor/roland/sd-90/07Gt_Org.mid.br" .
+cp -v "../download/vendor/yamaha/motif_es/TheLanes.mid.br" .
+cp -v "../download/vendor/yamaha/motif_es/TranceAct.mid.br" .
+#cp -v "../download/vendor/yamaha/plg_dx/12SOULDX.mid.br" .
+cp -v "../download/vendor/yamaha/plg_dx/12VOICE.mid.br" .
+cp -v "../download/vendor/yamaha/plg_vl/DinoJung.mid.br" .
+#cp -v "../download/vendor/yamaha/plg_pvl/BlkHole.mid.br" .
+cp -v "../download/vendor/yamaha/qy/QY100D_2.mid.br" .
+#cp -v "../download/vendor/yamaha/xgstudio/04Techno.mid.br" .
 brotli -v -dj *.br
 exit
