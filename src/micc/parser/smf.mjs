b446cfa1ff2189e2debde203327d4b2120e40de0
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2026-09-15 10:20:27 +0000

    MICC: Patched SMF regulator. Still requires a re-implementation. Bug spotted and raised indirectly by @PurpBatBoi.

--- src/micc/parser/smf.mjs
+++ src/micc/parser/smf.mjs
@@ -661,112 +661,141 @@ export default class MICCInternalsSMF {
 			case "MTrk":
 			case "XFIH":
 			case "XFKM": {
-				let eventContext = subchunk.context;
-				delete eventContext.statusPos;
-				delete eventContext.sizePos; // f0, ff
-				delete eventContext.dataPos; // f0, ff
-				let deltaSize = IntegerHandler.sizeVLV(subchunk.data, offset);
-				let remainingSize = subchunk.data.length - offset;
-				if (deltaSize <= 0 || deltaSize > 4) {
-					if (deltaSize === 0 && remainingSize < 4) {
+				const eventContext = subchunk.context;
+				const remainingSize = subchunk.data.length - offset;
+				// This current implementation is incredibly flawed for not handling chunk boundary crossing by itself. A new implementation is thus required.
+				if (eventContext.remaining > 0) {
+					// Size of event already known.
+					if (!(eventContext.expected > 0)) {
+						throw(new Error(`Incomplete event read continuation without expected data size.`));
+					};
+					if (remainingSize < eventContext.remaining) {
+						eventContext.remaining -= remainingSize;
+						console.debug(`Event 0x${eventContext.status.toString(16)} buffer incomplete: ${eventContext.expected} B expected, ${eventContext.remaining} B remaining.`);
+						return 0;
+					} else {
+						console.debug(`Event 0x${eventContext.status.toString(16)} buffer complete: ${eventContext.expected} B expected.`);
+						const remaining = eventContext.remaining;
+						eventContext.remaining = 0;
+						eventContext.expected = 0;
+						return remaining;
+					};
+				} else {
+					delete eventContext.statusPos;
+					delete eventContext.sizePos; // f0, ff
+					delete eventContext.dataPos; // f0, ff
+					const deltaSize = IntegerHandler.sizeVLV(subchunk.data, offset);
+					if (deltaSize <= 0 || deltaSize > 4) {
+						if (deltaSize === 0 && remainingSize < 4) {
+							console.debug(`Event read incomplete: Delta time read incomplete.`);
+							return 0;
+						};
+						throw(new RangeError(`Delta time is invalid at 0x${(subchunk.offsetData + offset).toString(16).padStart(6, "0")}`));
+					};
+					if (deltaSize >= remainingSize) {
+						console.debug(`Event read incomplete: Delta time size incomplete.`); // When will this be true?
 						return 0;
 					};
-					throw(new RangeError(`Delta time is invalid at 0x${(subchunk.offsetData + offset).toString(16).padStart(6, "0")}`));
-				};
-				if (deltaSize >= remainingSize) {
-					return 0;
-				};
-				let statusPos = offset + deltaSize;
-				eventContext.statusPos = deltaSize;
-				let fullStatusPos = statusPos + subchunk.offsetData;
-				let statusByte = 0, isStale = false;
-				if (subchunk.data[statusPos] & 0x80) {
-					// Status byte.
-					statusByte = subchunk.data[statusPos];
-					eventContext.status = statusByte;
-					//this.debugMode && console.debug(`Status (fresh): ${statusByte.toString(16)}`);
-				} else {
-					// Re-use running status.
-					if ((subchunk.offset + offset) === 0) {
-						throw(new Error(`Stale running status should never be at the start of the chunk at 0x${fullStatusPos.toString(16).padStart(6, "0")}`));
-					} else if (eventContext.status >= 0xf0) {
-						throw(new Error(`Stale running status should never be ${eventContext.status.toString(16)} at 0x${fullStatusPos.toString(16).padStart(6, "0")}`));
+					const statusPos = offset + deltaSize;
+					eventContext.statusPos = deltaSize;
+					const fullStatusPos = statusPos + subchunk.offsetData;
+					let statusByte = 0, isStale = false;
+					if (subchunk.data[statusPos] & 0x80) {
+						// Status byte.
+						statusByte = subchunk.data[statusPos];
+						eventContext.status = statusByte;
+						//this.debugMode && console.debug(`Status (fresh): ${statusByte.toString(16)}`);
 					} else {
-						statusByte = eventContext.status;
-						isStale = true;
-						//this.debugMode && console.debug(`Status (stale): ${statusByte.toString(16)}`);
-					};
-				};
-				let fullSize = deltaSize;
-				switch (statusByte) {
-					case 0xf0:
-					case 0xf7: {
-						// SysEx and SysEx continuation.
-						let seSizeSize = IntegerHandler.sizeVLV(subchunk.data, offset + deltaSize + 1);
-						let seRSize = remainingSize - deltaSize - 1;
-						if (seSizeSize <= 0 || seSizeSize > 4) {
-							if (seSizeSize === 0 && seRSize < 4) {
-								return 0;
-							};
-							throw(new Error(`SysEx size is invalid at 0x${(subchunk.offsetData + offset).toString(16).padStart(6, "0")}`));
-						};
-						eventContext.sizePos = deltaSize + 1;
-						eventContext.dataPos = eventContext.sizePos + seSizeSize;
-						fullSize += 1 + seSizeSize + IntegerHandler.readVLV(subchunk.data, offset + deltaSize + 1);
-						break;
-					};
-					case 0xff: {
-						// Metadata.
-						let mdSizeSize = IntegerHandler.sizeVLV(subchunk.data, offset + deltaSize + 2);
-						let mdRSize = remainingSize - deltaSize - 2;
-						if (mdSizeSize <= 0 || mdSizeSize > 4) {
-							if (mdSizeSize === 0 && mdRSize < 4) {
-								return 0;
-							};
-							throw(new Error(`Metadata size is invalid at 0x${(subchunk.offsetData + offset).toString(16).padStart(6, "0")}`));
-						};
-						eventContext.sizePos = deltaSize + 2;
-						eventContext.dataPos = eventContext.sizePos + mdSizeSize;
-						fullSize += 2 + mdSizeSize + IntegerHandler.readVLV(subchunk.data, offset + deltaSize + 2);
-						break;
-					};
-					default: {
-						switch (statusByte >> 4) {
-							case 8:
-							case 9:
-							case 10:
-							case 11:
-							case 14: {
-								// Normal events.
-								fullSize += isStale ? 2 : 3;
-								break;
-							};
-							case 12:
-							case 13: {
-								// Normal events.
-								fullSize += isStale ? 1 : 2;
-								break;
-							};
-							case 15: {
-								throw(new Error(`Unknown SMF status 0x${statusByte.toString(16)} at 0x${(fullStatusPos).toString(16).padStart(6, "0")}.`));
-								break;
-							};
-							default: {
-								// Malformed SMF data!
-								throw(new Error(`SMF data malformed at 0x${(fullStatusPos).toString(16).padStart(6, "0")}.`));
-							};
+						// Re-use running status.
+						if ((subchunk.offset + offset) === 0) {
+							throw(new Error(`Stale running status should never be at the start of the chunk at 0x${fullStatusPos.toString(16).padStart(6, "0")}`));
+						} else if (eventContext.status >= 0xf0) {
+							throw(new Error(`Stale running status should never be ${eventContext.status.toString(16)} at 0x${fullStatusPos.toString(16).padStart(6, "0")}`));
+						} else {
+							statusByte = eventContext.status;
+							isStale = true;
+							//this.debugMode && console.debug(`Status (stale): ${statusByte.toString(16)}`);
 						};
 					};
+					let fullSize = deltaSize;
+					switch (statusByte) {
+						case 0xf0:
+						case 0xf7: {
+							// SysEx and SysEx continuation.
+							let seSizeSize = IntegerHandler.sizeVLV(subchunk.data, offset + deltaSize + 1);
+							let seRSize = remainingSize - deltaSize - 1;
+							if (seSizeSize <= 0 || seSizeSize > 4) {
+								if (seSizeSize === 0 && seRSize < 4) {
+									console.debug(`Event 0x${statusByte.toString(16)} size read incomplete.`);
+									return 0;
+								};
+								throw(new Error(`SysEx size is invalid at 0x${(subchunk.offsetData + offset).toString(16).padStart(6, "0")}`));
+							};
+							eventContext.sizePos = deltaSize + 1;
+							eventContext.dataPos = eventContext.sizePos + seSizeSize;
+							fullSize += 1 + seSizeSize + IntegerHandler.readVLV(subchunk.data, offset + deltaSize + 1);
+							break;
+						};
+						case 0xff: {
+							// Metadata.
+							let mdSizeSize = IntegerHandler.sizeVLV(subchunk.data, offset + deltaSize + 2);
+							let mdRSize = remainingSize - deltaSize - 2;
+							if (mdSizeSize <= 0 || mdSizeSize > 4) {
+								if (mdSizeSize === 0 && mdRSize < 4) {
+									console.debug(`Event 0x${statusByte.toString(16)} size read incomplete.`);
+									return 0;
+								};
+								throw(new Error(`Metadata size is invalid at 0x${(subchunk.offsetData + offset).toString(16).padStart(6, "0")}`));
+							};
+							eventContext.sizePos = deltaSize + 2;
+							eventContext.dataPos = eventContext.sizePos + mdSizeSize;
+							fullSize += 2 + mdSizeSize + IntegerHandler.readVLV(subchunk.data, offset + deltaSize + 2);
+							break;
+						};
+						default: {
+							switch (statusByte >> 4) {
+								case 8:
+								case 9:
+								case 10:
+								case 11:
+								case 14: {
+									// Normal events.
+									fullSize += isStale ? 2 : 3;
+									break;
+								};
+								case 12:
+								case 13: {
+									// Normal events.
+									fullSize += isStale ? 1 : 2;
+									break;
+								};
+								case 15: {
+									throw(new Error(`Unknown SMF status 0x${statusByte.toString(16)} at 0x${(fullStatusPos).toString(16).padStart(6, "0")}.`));
+									break;
+								};
+								default: {
+									// Malformed SMF data!
+									throw(new Error(`SMF data malformed at 0x${(fullStatusPos).toString(16).padStart(6, "0")}.`));
+								};
+							};
+						};
+					};
+					if (remainingSize < fullSize) {
+						eventContext.expected = fullSize;
+						eventContext.remaining = fullSize - remainingSize;
+						console.debug(`Event 0x${statusByte.toString(16)} read incomplete: ${fullSize} B expected, ${eventContext.remaining} B remaining.`);
+						return 0;
+					};
+					eventContext.expected = 0;
+					eventContext.remaining = 0;
+					console.debug(`Event 0x${statusByte.toString(16)} read complete: ${fullSize} B total.`);
+					return fullSize;
 				};
-				if (remainingSize < fullSize) {
-					return 0;
-				};
-				//this.debugMode && console.debug(`0x${(subchunk.offsetData + offset).toString(16).padStart(6, "0")} (${offset}): ${deltaSize} %o`, subchunk.data.subarray(offset, offset + fullSize));
-				return fullSize;
 				break;
 			};
 			case "MThd":
 			default: {
+				// Buffer everything.
 				return 0;
 			};
 		};
